7f5019d0c028e10cf0aaf75191806b56d9414840
Bruno <bruno.pleysier@gmail.com>
2021-12-03 22:10:41 +0100

    Security folder + imports

---
+++ src/main/java/security/JwtUtil.java
@@ -0,0 +1,17 @@
+package security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+public class JwtUtil {
+}
